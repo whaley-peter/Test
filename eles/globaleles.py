436a3122618b238f1d8762911c9d9ed8eaea20b7
whaley-peter <18207168761@163.com>
2017-08-14 17:40:04 +0800

    将cfg.py文件合并到globaleles中

--- eles/globaleles.py
+++ eles/globaleles.py
@@ -4,6 +4,33 @@
 __mtime__ = '2017/8/14'
 """全局变量，多处通用"""
 #by.id
+#返回按钮
 backbutton = "com.snailvr.manager:id/btn_back"
 
+#标题栏
 title = "com.snailvr.manager:id/tv_title"
+
+
+from libs.launchManagerment import getProjectRootPath
+apppath = getProjectRootPath()+ r"\app\WhaleyVR.apk"
+
+desired_caps = {
+    'platformName':'Android',
+    'platformVersion' : '6.0',
+    'deviceName' :'test',
+    'udid':'6221231716B0904714',
+    'app' : apppath,
+    'appPackage' : 'com.snailvr.manager',
+    'appActivity' : 'com.whaley.vr.module.launcher.activitys.SplashActivity',
+    'unicodeKeyborad' : True,
+    'resetKeyborad' : True,
+    'noRest' : True,
+    'commandTimeout': 60,
+    'autoGrantPermissions':True,
+    'sessonOverride': True
+}
+
+remote_server = 'http://localhost:4723/wd/hub'
+username = '18616512272'
+password = 'a123456'
+
