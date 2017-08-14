5d5dd0cfce1e82b7c2677b48e313ee3fce019fa7
whaley-peter <18207168761@163.com>
2017-08-14 13:04:28 +0800

    commit globaleles.py

---
+++ eles/globaleles.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__mtime__ = '2017/8/14'
+"""全局变量，多处通用"""
+#by.id
+backbutton = "com.snailvr.manager:id/btn_back"
+
+title = "com.snailvr.manager:id/tv_title"
