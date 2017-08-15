9cb6af323f1575fba586a1cf4f1ada2ae3d26692
RedsChris <cai.shengwei@whaley.cn>
2017-08-15 15:13:49 +0800

    修改desired caps

--- eles/globaleles.py
+++ eles/globaleles.py
@@ -16,9 +16,9 @@ apppath = getProjectRootPath()+ r"\app\WhaleyVR.apk"
 
 desired_caps = {
     'platformName':'Android',
-    'platformVersion' : '6.0',
+    'platformVersion' : '7.0',
     'deviceName' :'test',
-    'udid':'6221231716B0904714',
+    'udid':'GWY0217115007494',
     'app' : apppath,
     'appPackage' : 'com.snailvr.manager',
     'appActivity' : 'com.whaley.vr.module.launcher.activitys.SplashActivity',
@@ -31,6 +31,6 @@ desired_caps = {
 }
 
 remote_server = 'http://localhost:4723/wd/hub'
-username = '18616512272'
-password = 'a123456'
+username = '13764205429'
+password = 'lang123456'
 
