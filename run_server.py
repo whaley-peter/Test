651c3015dd3f04ebf4b732183e65f2d97a25d54f
whaley-peter <18207168761@163.com>
2017-08-21 18:16:16 +0800

    创建模块，用于启动多个appium服务

---
+++ run_server.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from mutil_test import get_info
+from mutil_test import manage_server
+import sys
+import time
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+ip=get_info.get_ip()
+arglist=sys.argv
+
+if '-h' in arglist:
+    outstr = '''
+    how to use it
+    -h  help
+    -n the num of starting appium server
+    -p server port
+    -sp decond server port
+    '''
+    print outstr
+    sys.exit(0)
+
+if "-n" in arglist:
+    num=arglist[arglist.index('-n')+1]
+else:
+    num=0
+
+if "-p" in arglist:
+    aport=int(arglist[arglist.index('-p')+1])
+    iport=int(arglist[arglist.index('-p')+1])
+else:
+    aport=4723
+    iport=14723
+
+if "-sp" in arglist:
+    bport=int(arglist[arglist.index('-sp')+1])
+    wport=int(arglist[arglist.index('-sp')+1])
+else:
+    bport=5723
+    wport=8101
+
+
+def start_server():
+    global aport
+    global bport
+    global wport
+    global iport
+    global ip
+    global plat
+
+    result = manage_server.kill_port(aport)
+    manage_server.kill_port(bport)
+
+    run_appium = 'start .\\mutil_test\\run_appium_server.bat {0} {1} {2}'.format(ip,aport,bport)
+    aport += 1
+    bport += 1
+    if result == 1:
+        os.system(run_appium)
+    else:
+        pass
+
+devicelist = get_info.get_devices()
+
+if devicelist==[]:
+    if int(num) == 0:
+        print "NO Android Device Connect The PC!"
+    else:
+        for t in range(int(num)):
+            start_server()
+else:
+    if int(num)==0:
+        for div in devicelist:
+            start_server()
+    else:
+        t2 = 0
+        for t2 in range(int(num)):
+            start_server()
+
+time.sleep(3)
