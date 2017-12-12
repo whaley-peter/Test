54ce0b8a08937956f2f7d399fd524b68a85d3c81
whaley-peter <18207168761@163.com>
2017-12-12 17:48:03 +0800

    新建我的关注界面元素

---
+++ eles/myattentionpage.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__mtime__ = '2017/12/11'
+from eles.homepage import historyempty
+#我的关注
+myattention = 'com.snailvr.manager:id/tv_discover'
+
+#已关注发布者
+attented_publishers = 'com.snailvr.manager:id/iv_icon'
+#已关注发布者为空
+attented_publishers_empty = historyempty
+
+#推荐关注按钮
+suggested_attention = 'com.snailvr.manager:id/titlebar_left'
+
+
+#推荐关注界面
+#发布者头像
+publishers_pic = "com.snailvr.manager:id/iv_avatar"
+publishers_name = 'com.snailvr.manager:id/tv_name'
+#关注发布者
+publishers_attented = 'com.snailvr.manager:id/view_follow'
